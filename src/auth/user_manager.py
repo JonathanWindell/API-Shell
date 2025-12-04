280301e8f7ab63d1b0517ae3cbd1c30f56e643a9
Jonathan Windell <windelljonathan04@gmail.com>
2025-12-04 18:24:37 +0100

    finished auth token

---
+++ src/auth/user_manager.py
@@ -0,0 +1,34 @@
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+import jwt
+
+# Create hashed password using bcrypt
+class user_manager:
+    # Constants for user token
+    SECRET_KEY = "SECRET_KEY"
+    ALGORITHM = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+    # Create hashing rules for password 
+    def __init__(self):
+        self.pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+    
+    # Get the password hash
+    def get_password_hash(self, password):
+        hashed_password = self.pwd_context.hash(password)
+
+        return hashed_password
+
+    def create_access_token(self, data: dict, expires_delta: timedelta = None):
+        to_encode = data.copy()
+
+        if expires_delta:
+            expire = datetime.now() + expires_delta
+        else:
+            expire = datetime.now() + timedelta(self.ACCESS_TOKEN_EXPIRE_MINUTES)
+
+        to_encode.update({"exp": expire})
+
+        encoded_jwt = jwt.encode(to_encode, self.SECRET_KEY, algorithm=self.ALGORITHM)
+
+        return encoded_jwt
--- tests/test_user_manager.py
+++ tests/test_user_manager.py
@@ -1,13 +1,29 @@
 import pytest
+import jwt
 
+from src.auth.user_manager import user_manager
+
+# Test user password is hashed correctly
 def test_user_password_hash():
-    pwd_context = UserManager()
+    pwd_context = user_manager()
     secret_password = "secret123"
 
     hashed_password = pwd_context.get_password_hash(secret_password)
 
-    assert password != hashed_password
+    assert secret_password != hashed_password
     assert hashed_password.startswith("$2b$")
 
     
+# Test create user access token
+def test_create_access_token():
+    manager = user_manager()
+    username = "testuser"
+
+    jwt_token = manager.create_access_token(data={"sub": username})
+
+    assert isinstance(jwt_token, str)
+    assert len(jwt_token) > 0
     
+    payload = jwt.decode(jwt_token, "SECRET_KEY", algorithms=["HS256"])
+    assert payload["sub"] == "testuser"
+
