b5e36ceb92e0c92efd74c358d9f80f56a0c2a33f
Jonathan Windell <windelljonathan04@gmail.com>
2025-12-31 11:00:26 +0100

    Safety commit. Started working on CI/CD flow

--- src/security/settings.py
+++ src/security/settings.py
@@ -7,14 +7,15 @@ load_dotenv()
 Application Configuration
 """
 
-# Get keys from .env file and set as enviroment variables
+# Get keys from .env file. Default to "test_key_1" if not found.
+# NOTE: In .env, this should be named API_KEYS (plural)
 _keys_string = os.getenv("API_KEYS", "test_key_1")
 
-# Split VALID_API_KEY to only get key
+# Split VALID_API_KEY to only get actual key 
 VALID_API_KEYS = {key.strip() for key in _keys_string.split(",")}
 
 # Rate limiting constants
-RATE_LIMIT_LIMIT = 3
-RATE_LIMIT_WINDOW = 60
+RATE_LIMIT_LIMIT = int(os.getenv("RATE_LIMIT_LIMIT", 3))
+RATE_LIMIT_WINDOW = int(os.getenv("RATE_LIMIT_WINDOW", 60))
 
 
