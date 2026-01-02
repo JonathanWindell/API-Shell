f34a03df0872e13562f0e92d0848acc786d931c7
Jonathan Windell <windelljonathan04@gmail.com>
2026-01-02 16:37:23 +0100

    Split main.py and created routes files for separation of concerns

---
+++ src/routers/auth.py
@@ -0,0 +1,40 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi import APIRouter
+from typing import Annotated
+
+from src.auth.user_manager import UserManager
+
+# Create FastAPI router
+router = APIRouter()
+
+# Dependency Injection
+def get_user_manager():
+    return UserManager()
+
+
+# Create route
+@router.post("/token")
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    user_manager: Annotated[UserManager, Depends(get_user_manager)],
+):
+    """
+    Login endpoint. authenticates user and returns a JWT token.
+    """
+    # Mock userbase
+    fake_db_user = "admin"
+    fake_db_hash = user_manager.get_password_hash("secret")
+
+    if form_data.username != fake_db_user or not user_manager.verify_password(
+        form_data.password, fake_db_hash
+    ):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    access_token = user_manager.create_access_token(data={"sub": form_data.username})
+
+    return {"access_token": access_token, "token_type": "bearer"}
---
+++ src/routers/general.py
@@ -0,0 +1,35 @@
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import APIRouter, Depends
+from typing import Annotated
+
+from src.auth.user_manager import UserManager
+from src.security.limiter import check_rate_limiter
+
+# Create FastAPI router
+router = APIRouter()
+
+# Dependency Injection
+def get_user_manager():
+    return UserManager()
+
+# Create OAuth2 scheme
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+# Create routes
+@router.get("/protected")
+async def read_protected_route(
+    _token: Annotated[str, Depends(oauth2_scheme)],
+    _allowed: bool = Depends(check_rate_limiter),
+):
+    """
+    A protected route. Requires a valid JWT token and is Rate Limited.
+    """
+    return {
+        "username": "admin",
+        "message": "You have successfully accessed a protected route!",
+    }
+
+
+@router.get("/")
+def health_check():
+    return {"status": "System is running", "docs": "Go to /docs to test the API"}
