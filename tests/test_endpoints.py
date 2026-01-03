c999a526653fe6dda89f52c550cac29fb4ae5eac
Jonathan Windell <windelljonathan04@gmail.com>
2026-01-03 12:05:33 +0100

    Added testcases for routers general and auth

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -27,3 +27,55 @@ def test_read_protected_route_no_token(client):
 
     # Assert
     assert response.status_code == 401
+
+
+def test_router_auth_return(client):
+    """
+    Tests return from auth for correct formatting and data for router /token
+    """
+
+    # Arrange
+    test_dict = {
+        "username": "admin",
+        "password": "secret",
+    }
+
+    # Act
+    response = client.post("/token", data=test_dict)
+
+    # Assert
+    assert response.status_code == 200
+    assert response.json()["access_token"]
+    assert response.json()["token_type"]
+
+
+def test_router_general_health_check_return(client):
+    """
+    Tests return health check for router /protected
+    """
+
+    # Act
+    response = client.get("/")
+
+    # Assert
+    assert response.status_code == 200
+    assert response.json()["status"]
+
+
+def test_login_failure(client):
+    """
+    Tests mocking of router /token
+    """
+
+    # Arrange
+    test_dict = {
+        "username": "admin",
+        "password": "wrong_secret",
+    }
+
+    # Act
+    response = client.post("/token", data=test_dict)
+
+    # Assert
+    assert response.status_code == 401
+    assert response.json()["detail"] == "Incorrect username or password"
